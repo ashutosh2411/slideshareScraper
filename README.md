073e02b7d690df601856a6cb3a423b28b2257cbd
okabe <ashutoshu2411@gmail.com>
2018-01-06 14:34:35 +0530

    Final commit.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # slideshareScraper
 A selenium based tool that scrapes the Presentations from slideshare and generates the corresponding pdf 
+
+Currently, to convert the images to pdf, open it in Impress or Power Point and import all images from the folder 'images' there and export as PDF or print the presentation.
+ 
+The project was a good investment of 3 hours. 
--- slideshare.py
+++ slideshare.py
@@ -1,10 +1,13 @@
 import urllib
 import time
+import os
+import sys
+from os import listdir
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
 driver = webdriver.Firefox()
 url = 'https://www.slideshare.net/anniyappa/dbms-an-example'
+folder = 'images/'
 driver.get(url)
 source = driver.page_source
 soup = BeautifulSoup(source, 'lxml')
@@ -12,12 +15,15 @@ elem = soup.findAll(True, {'class':['slide_image']})
 driver.quit()
 l = []
 count = 1
+file = [f for f in listdir(folder)]
+for f in file:
+	os.remove(folder+f)
+
 print len(elem)
 for i in elem :
 	t = str(i)
 	t = t[t.find('data-full')+11:]
 	temp = t[:t.find('"')]
 	l.append(temp)
-	urllib.urlretrieve(temp, str(count).zfill(3)+".jpg")
+	urllib.urlretrieve(temp, folder + str(count).zfill(3)+".jpg")
 	count = count + 1
-print l
